715745843ee818ad8a70df5dcb23c9bce8f894f0
2nn300hu <2nn300hu@gmail.com>
2020-10-23 14:09:24 +0900

    Use readInt(), readUnsignedByte() instead of read()

--- neuralnetwork/datasetload/LoadMNIST.java
+++ neuralnetwork/datasetload/LoadMNIST.java
@@ -8,53 +8,36 @@ import java.io.FileInputStream;
 
 
 public class LoadMNIST {
-    private static int changeByteToInt(byte[] data) {
-        int temp = 0;
-        for (byte i : data) {
-            temp = (temp << 8) + i;
-        }
-        return temp;
-    }
-
-    private static int changeUnsignedByteToInt(byte[] data) {
-        int temp = 0;
-        for (byte i : data) {
-            temp = (temp << 8) + i;
-        }
-        return temp & 0xff;
-    }
-
     public static Dataset load(String dataFile, String labelFile) throws Exception {
         Dataset dataset = new Dataset();
-        byte[] magicNumber = new byte[4];
-        byte[] itemSize = new byte[4];
-        byte[] rowSize = new byte[4];
-        byte[] columnSize = new byte[4];
-        byte[] label = new byte[1];
-        byte[] pixel = new byte[1];
+        int magicNumber;
+        int itemSize;
+        int rowSize;
+        int columnSize;
+
         DataInputStream imagedata = new DataInputStream(new FileInputStream(dataFile));
         DataInputStream labeldata = new DataInputStream(new FileInputStream(labelFile));
 
-        imagedata.read(magicNumber);
-        imagedata.read(itemSize);
-        imagedata.read(rowSize);
-        imagedata.read(columnSize);
+        magicNumber = imagedata.readInt();
+        itemSize = imagedata.readInt();
+        rowSize = imagedata.readInt();
+        columnSize = imagedata.readInt();
 
-        if (changeByteToInt(magicNumber) != 2051) {
+        if (magicNumber != 2051) {
             throw new Exception("Data file is not Valid :" + dataFile);
         }
 
-        MNISTFile imagedataFile = new MNISTFile(changeByteToInt(rowSize), changeByteToInt(columnSize), changeByteToInt(itemSize));
+        MNISTFile imagedataFile = new MNISTFile(rowSize, columnSize, itemSize);
         System.out.printf("Image data loaded; image size : %d count : %d \n", imagedataFile.dataSize, imagedataFile.dataCount);
 
-        labeldata.read(magicNumber);
-        labeldata.read(itemSize);
+        magicNumber = labeldata.readInt();
+        itemSize = labeldata.readInt();
 
-        if (changeByteToInt(magicNumber) != 2049) {
+        if (magicNumber != 2049) {
             throw new Exception("Label file is not Valid :" + dataFile);
         }
 
-        MNISTFile labeldataFile = new MNISTFile(changeByteToInt(itemSize));
+        MNISTFile labeldataFile = new MNISTFile(itemSize);
         System.out.printf("Label data loaded; count : %d\n", labeldataFile.dataCount);
 
         if (labeldataFile.dataCount != imagedataFile.dataCount) {
@@ -63,15 +46,23 @@ public class LoadMNIST {
 
         int tempLabel;
         int[] tempImage = new int[imagedataFile.dataSize];
+
         for (int k = 0; k < imagedataFile.dataCount; k++) {
-            labeldata.read(label);
-            tempLabel = changeUnsignedByteToInt(label);
+            tempLabel = labeldata.readUnsignedByte();
             for (int i = 0; i < imagedataFile.dataSize; i++) {
-                imagedata.read(pixel);
-                tempImage[i] = changeUnsignedByteToInt(pixel);
+                tempImage[i] = imagedata.readUnsignedByte();
             }
             Data tempData = new Data(tempLabel, tempImage);
             dataset.addData(tempData);
+//        //Test code for printing one image
+//        int k = 0;
+//        for (int i : tempData.getData()) {
+//            System.out.printf("%d " ,i);
+//            k = (k+1) % 28;
+//            if (k ==0){
+//                System.out.println();
+//            }
+//        }
         }
 
         return dataset;
