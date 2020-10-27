dba0b4044d88f1b62bacd09aa721d6d87851fb0e
2nn300hu <2nn300hu@gmail.com>
2020-10-27 23:12:55 +0900

    Implement He initialization

---
+++ neuralnetwork/initializemethod/HeInitialization.java
@@ -0,0 +1,44 @@
+package neuralnetwork.initializemethod;
+
+import neuralnetwork.layer.LayerData;
+import neuralnetwork.machinelearning.NeuralNetwork;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class HeInitialization implements InitializeMethod {
+    private Random random;
+
+    public HeInitialization() {
+        long seed = System.currentTimeMillis();
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public ArrayList<LayerData> set(NeuralNetwork neuralNetwork) {
+        ArrayList<LayerData> layerDataArrayList = new ArrayList<>();
+        ArrayList<int[]> layerSize = neuralNetwork.getLayerSize();
+        for (int[] size : layerSize) {
+            LayerData temp = new LayerData();
+            temp.inputSize = size[0];
+            temp.outputSize = size[1];
+            temp.weight = new double[size[0]][size[1]];
+            temp.bias = new double[size[1]];
+            for (int k = 0; k < size[0]; k++) {
+                for (int j = 0; j < size[1]; j++) {
+                    temp.weight[k][j] = he(size[0]);
+                }
+
+            }
+            for (int j = 0; j < size[1]; j++) {
+                temp.bias[j] = he(size[0]);
+            }
+            layerDataArrayList.add(temp);
+        }
+        return layerDataArrayList;
+    }
+
+    private double he(int in) {
+        return (random.nextDouble() - 0.5) * 2 * Math.sqrt(6.0 / in);
+    }
+}
