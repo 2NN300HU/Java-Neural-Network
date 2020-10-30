18652ae9d372bfeb027a8eaccd32b81726e6d7c9
2nn300hu <2nn300hu@gmail.com>
2020-10-30 23:44:56 +0900

    Improve InputNormalize()

--- neuralnetwork/inputfunction/InputNormalize.java
+++ neuralnetwork/inputfunction/InputNormalize.java
@@ -4,9 +4,21 @@ public class InputNormalize implements InputFunction {
     @Override
     public double[][] function(double[][] input) {
         double[][] result = new double[input.length][input[0].length];
+        double min;
+        double max;
         for (int i = 0; i < input.length; i++) {
+            max = input[i][0];
+            min = input[i][0];
+            for (double j : input[i]) {
+                if (max < j) {
+                    max = j;
+                } else if (min > j) {
+                    min = j;
+                }
+            }
+            double temp = max - min;
             for (int j = 0; j < input[0].length; j++) {
-                result[i][j] = input[i][j] / 255;
+                result[i][j] = input[i][j] / temp;
             }
         }
         return result;
